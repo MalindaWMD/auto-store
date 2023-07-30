1d2acf26c7ec769b4eb7548dab3cbd3f9f4e2c2a
Malinda <malinda.weerasinghe@betmakers.com>
2023-07-30 21:35:08 +0530

    Add noindex

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="robots" content="noindex">
+    <meta name="googlebot" content="noindex">
+
     <title>{{config('app.name')}}</title>
 
     <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
